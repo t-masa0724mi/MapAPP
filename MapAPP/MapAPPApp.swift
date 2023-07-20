edaa5312b92a16af785e7f46b1f78c3616a15f05
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-20 15:29:24 +0900

    Initial Commit

---
+++ MapAPP/MapAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAPPApp.swift
+//  MapAPP
+//
+//  Created by 飯塚政美 on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct MapAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
