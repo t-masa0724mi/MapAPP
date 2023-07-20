df4c0851e2172c25ec456f9e02c14c76709bc9a2
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-20 16:00:46 +0900

    マップアプリ作成

--- MapAPP/ContentView.swift
+++ MapAPP/ContentView.swift
@@ -1,14 +1,61 @@
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.334_900, longitude: -122.009_020),
+        latitudinalMeters: 750,
+        longitudinalMeters: 750
+    )
+    
+    @ State var place: IdentifiablePlace = IdentifiablePlace(lat: 37.334_900, long: 122.009_020)
+    
+    @State var address = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("アドレスを入れてください", text: $address)
+            
+            Button(action: {
+                DoCoding(address: address)
+            }, label: {
+                Text("Search")
+            })
+            Map(coordinateRegion: $region,
+                annotationItems: [place])
+            { place in
+                MapPin(coordinate: place.location,
+                       tint: Color.red)
+            }
         }
-        .padding()
+    }
+    
+    func DoCoding(address: String) {
+        CLGeocoder().geocodeAddressString(address) { placemarks, error in
+            if let coordinate = placemarks?.first?.location?.coordinate {
+                region = MKCoordinateRegion(
+                    center: CLLocationCoordinate2D(
+                        latitude: coordinate.latitude,
+                        longitude: coordinate.longitude
+                    ),
+                    latitudinalMeters: 750,
+                    longitudinalMeters: 750)
+                
+                place = IdentifiablePlace(lat: coordinate.latitude, long: coordinate.longitude)
+            }
+        }
+    }
+}
+
+struct IdentifiablePlace: Identifiable {
+    let id: UUID
+    let location: CLLocationCoordinate2D
+    init(id: UUID = UUID(), lat: Double, long: Double) {
+        self.id = id
+        self.location = CLLocationCoordinate2D(
+            latitude: lat,
+            longitude: long)
     }
 }
 
